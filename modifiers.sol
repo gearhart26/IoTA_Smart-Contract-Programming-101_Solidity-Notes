e0b446a60e825751927e5133d1b8de0a38985ded
Dustin Gearhart <gearhart26@gmail.com>
2021-07-27 11:45:13 -0700

    Update modifiers.sol

--- modifiers.sol
+++ modifiers.sol
@@ -4,3 +4,11 @@
 //way to seperate and reuse logic that is going to be used repeatadly throughout contract
 
 pragma solidity 0.7.5;
+    
+//reusing code from Require&Assert
+    
+    contract Bank_ModifierExample{
+        mapping(address => uint) balance;
+        address owner;
+        
+//New code here
