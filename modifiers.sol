c6166b55aa537d78486ca09653aaf0e20ad67756
Dustin Gearhart <gearhart26@gmail.com>
2021-07-27 11:44:47 -0700

    Update modifiers.sol

--- modifiers.sol
+++ modifiers.sol
@@ -1,1 +1,6 @@
+// MODIFIERS //
 
+// modifiers = smaller function that always runs just before a real function
+//way to seperate and reuse logic that is going to be used repeatadly throughout contract
+
+pragma solidity 0.7.5;
