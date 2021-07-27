8c1519efa760d0f045717ed793d087e646874cb9
Dustin Gearhart <gearhart26@gmail.com>
2021-07-27 11:47:04 -0700

    Update modifiers.sol

--- modifiers.sol
+++ modifiers.sol
@@ -22,3 +22,29 @@ pragma solidity 0.7.5;
             // basicly signifies the modifier is done, run the function body
             _;
         }
+        
+        constructor(){
+            owner = msg.sender;
+        }
+        
+        //adding modifier function onlyOwner to head of function instead of using require() in the body
+        function addBalance(uint _balanceToAdd) public onlyOwner returns(uint){
+            balance[msg.sender] += _balanceToAdd; 
+            return balance[msg.sender];
+        }
+        
+        function getBalance() public view returns (uint){
+            return balance[msg.sender];
+        }
+        
+        function transfer(address recipient, uint amount) public {
+            require(balance[msg.sender] >= amount, "Insufficent Balance");
+            require(msg.sender != recipient, "You cannot send funds to yourself");
+            _transfer(msg.sender, recipient, amount);
+        }
+        
+        function _transfer(address from, address to, uint amount) private {
+            balance[from] -= amount;
+            balance[to] += amount;
+        }
+    }
