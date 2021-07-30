c45529f758e217005068cc84664886d9183a8ce5
Dustin Gearhart <gearhart26@gmail.com>
2021-07-30 09:52:59 -0700

    Update payableFunctions.sol

--- payableFunctions.sol
+++ payableFunctions.sol
@@ -3,3 +3,23 @@
 
 //reusing code from Events
 pragma solidity 0.7.5;
+    
+    contract Bank_PayableFunctionsExample{
+        
+        mapping(address => uint) balance;
+        
+        address owner;
+
+//New code here; editing event 
+        event depositDone (uint amount, address depositedTo);
+        
+        event balanceTransfered (uint amount, address transferedTo);
+        
+        modifier onlyOwner{
+            require(msg.sender == owner);
+            _;
+        }
+        
+        constructor(){
+            owner = msg.sender;
+        }
