136e92c7d800f961724d399e16fd91f1c8d73619
Dustin Gearhart <gearhart26@gmail.com>
2021-07-31 09:48:51 -0700

    Update payableFunctions.sol

--- payableFunctions.sol
+++ payableFunctions.sol
@@ -23,3 +23,16 @@ pragma solidity 0.7.5;
         constructor(){
             owner = msg.sender;
         }
+        
+//New code here; changing the addBalance function
+        //need to add payable keyword to function head so the contract can hold funds
+        function deposit() public payable returns (uint) {
+            //The line below is no longer necessary since the balance held by the smart contract is stored on the blockchain. Therefore we do not need to save it to the smart contract
+            //basicly only keeps track internally of which addresses deposited what money
+            balance[msg.sender] += msg.value; 
+            emit depositDone(msg.value, msg.sender);
+            return balance[msg.sender];
+        }
+        
+        
+ }
