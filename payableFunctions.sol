cd79083ea6a7f406e2e8649aabb06bf5d1ff54c7
Dustin Gearhart <gearhart26@gmail.com>
2021-08-01 01:45:32 -0700

    Update payableFunctions.sol

--- payableFunctions.sol
+++ payableFunctions.sol
@@ -1,5 +1,5 @@
 // PAYABLE FUNCTIONS //
-//Writing our first smart contract that can actually hold real crypto currency
+//Writing our first smart contract that can actually hold crypto currency
 
 //reusing code from Events
 pragma solidity 0.7.5;
@@ -34,5 +34,33 @@ pragma solidity 0.7.5;
             return balance[msg.sender];
         }
         
+//New code here; new withdraw function
+        //This only allows withdraws to your own address but can be easily changed to withdraw to any address
+        function withdraw(uint amount) public returns (uint) {
+            //address payable toSend = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;      <= another way to do it 
+            // toSend.transfer(amount);                                                 <= another way to do it 
+            
+            //".transfer" is the safest way to do it in solidity
+            //the ".transfer" function is great because if the transfer fails it will throw an error and revert the transaction just like if we added a require() ststement
+            msg.sender.transfer(amount);       // msg.sender is an address
+            //This function works but the major flaw with our".transfer" method is that anyone who made a deposit can withdraw all funds from the smart contract reguardless of who deposited the funds
+        }
         
- }
+        function getBalance() public view returns (uint){
+            return balance[msg.sender];
+        }
+        
+        function transfer(address recipient, uint amount) public {
+            require(balance[msg.sender] >= amount, "Insufficent Balance");
+            require(msg.sender != recipient, "You cannot send funds to yourself");
+            uint previousSenderBalance = balance[msg.sender];
+            _transfer(msg.sender, recipient, amount);
+            assert(balance[msg.sender] == previousSenderBalance - amount);
+        }
+        
+        function _transfer(address from, address to, uint amount) private {
+            balance[from] -= amount;
+            balance[to] += amount;
+            emit balanceTransfered(amount, to);
+        }
+    }
