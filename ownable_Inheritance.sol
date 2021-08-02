5175543fe2c52e194fcab8f1505b6b7bc2f95096
Dustin Gearhart <gearhart26@gmail.com>
2021-08-02 16:43:27 -0700

    Update ownable_Inheritance.sol

--- ownable_Inheritance.sol
+++ ownable_Inheritance.sol
@@ -1,1 +1,16 @@
+pragma solidity 0.7.5;
 
+    contract Ownable {
+
+        //once a state variable is made public it will automaticly create a function to get the value of that variable which can be super helpful and save alot of time
+        address public owner;
+        
+         modifier onlyOwner{
+            require(msg.sender == owner);
+            _;
+        }
+        
+        constructor(){
+            owner = msg.sender;
+        }
+}
