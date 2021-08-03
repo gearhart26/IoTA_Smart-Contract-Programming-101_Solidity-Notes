868d9110fb0f34739ae8e51ed7d31cef3414d71d
Dustin Gearhart <gearhart26@gmail.com>
2021-08-03 11:32:10 -0700

    Update ownable_Internal.sol

--- ownable_Internal.sol
+++ ownable_Internal.sol
@@ -1,3 +1,22 @@
 // "OWNABLE_INTERNAL" BASE CONTRACT //
 //Reusing code from ownable inheritance base contract
 //Used with destroyable.sol from projects folder
+
+pragma solidity 0.7.5;
+
+    contract Ownable {
+
+//New Code Here
+        //Internal is similar private, it is only viewable within your contract or from any contract that is inheriting from it
+        //Prevents anyone other than certain contracts from querying the variable
+        address internal owner;
+        
+         modifier onlyOwner{
+            require(msg.sender == owner);
+            _;
+        }
+        
+        constructor(){
+            owner = msg.sender;
+        }
+    }
