e2d8843ff89eab544623907233e7b4fe486101af
Dustin Gearhart <gearhart26@gmail.com>
2021-08-04 00:58:32 -0700

    Update government_External.sol

--- government_External.sol
+++ government_External.sol
@@ -1,1 +1,7 @@
 // EXTERNAL CONTRACTS EXAMPLE //
+//External visibility level is how we are going to interact with other contracts 
+//Example: connecting your contract to uniswap to tap into their liquidy 
+
+//This contract is going to be an external contract that we are going to interact with through our ownable, destroyable, and bank contracts
+
+pragma solidity 0.7.5;
