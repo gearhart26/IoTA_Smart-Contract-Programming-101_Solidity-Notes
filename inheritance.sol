56ee82019bb6f4a60e1621cccb570ee43737fd97
Dustin Gearhart <gearhart26@gmail.com>
2021-08-05 17:09:20 -0700

    Update inheritance.sol

--- inheritance.sol
+++ inheritance.sol
@@ -10,5 +10,41 @@ import "./ownable_Inheritance.sol";
 
 //reusing code from PayableFunctions. This will act as the child contract 
     contract Bank_InheritanceExample is Ownable{
-    
+        
+        mapping(address => uint) balance;
+        
+        event depositDone (uint amount, address depositedTo);
+        
+        event balanceTransfered (address transferedFrom, uint amount, address transferedTo);
+        
+        function deposit() public payable returns (uint) {
+            balance[msg.sender] += msg.value; 
+            emit depositDone(msg.value, msg.sender);
+            return balance[msg.sender];
+        }
+        
+        function withdraw(uint amount) public returns (uint) {
+            require(balance[msg.sender] >= amount, "Insufficent Balance");
+            balance[msg.sender] -= amount;
+            msg.sender.transfer(amount);
+            return balance[msg.sender];
+       }
+        
+        function getBalance() public view returns (uint){
+            return balance[msg.sender];
+        }
+        
+        function transfer(address recipient, uint amount) public {
+            require(balance[msg.sender] >= amount, "Insufficent Balance");
+            require(msg.sender != recipient, "You cannot send funds to yourself");
+            uint previousSenderBalance = balance[msg.sender];
+            _transfer(msg.sender, recipient, amount);
+            assert(balance[msg.sender] == previousSenderBalance - amount);
+            emit balanceTransfered(msg.sender, amount, recipient);
+        }
+        
+        function _transfer(address from, address to, uint amount) private {
+            balance[from] -= amount;
+            balance[to] += amount;
+        }
     }
