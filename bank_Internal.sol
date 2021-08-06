7fc73501a84cb6f1bbc3bf088aa41d00a54e02d6
Dustin Gearhart <gearhart26@gmail.com>
2021-08-06 10:09:58 -0700

    Update bank_Internal.sol

--- bank_Internal.sol
+++ bank_Internal.sol
@@ -1,3 +1,8 @@
 // BANK_INTERNAL CONTRACT //
 //Reusing code from Inheritance
 //Used with destroyable.sol from projects folder
+
+import "./Solidity_101_Projects&Assignments/Destroyable.sol";
+import "./Solidity_101_Projects&Assignments/Ownable.sol";
+
+pragma solidity 0.7.5;
