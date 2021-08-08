af683713355a925c06dfd9f020d3138351f18dab
Dustin Gearhart <gearhart26@gmail.com>
2021-08-08 13:30:18 -0700

    Update bank_External.sol

--- bank_External.sol
+++ bank_External.sol
@@ -1,1 +1,8 @@
+// BANK_EXTERNAL CHILD CONTRACT //
+//Reusing code from Bank_internal
+//To be used with Destroyable.sol & Ownable.sol in projects&Assignments
 
+import "./Solidity_101_Projects&Assignments/Destroyable.sol";
+import "./Solidity_101_Projects&Assignments/Ownable.sol";
+
+pragma solidity 0.7.5;
